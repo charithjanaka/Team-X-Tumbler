e33b7c287bc370d22b7297d9b8785c05335508f0
Charith Janaka <64123653+charithjanaka@users.noreply.github.com>
2021-06-25 23:27:25 +0530

    Moved some files into Virtual Competition folder
    @Diluwas

---
+++ Virtual Competition/Robot_With_Arena/V_1.0/controllers/GateController/build/release/GateController.d
@@ -0,0 +1,6 @@
+build/release/GateController.o: GateController.cpp \
+ C:\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Webots/include/controller/c/webots/types.h \
+ C:\Webots/include/controller/cpp/webots/PositionSensor.hpp
